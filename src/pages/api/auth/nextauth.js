1b4e7183bf208837f5530ce7bd355d78b1d599dc
Okami <thefoxpaw@protonmail.com>
2023-06-01 23:56:01 -0500

    Added nextauth and authentication API

---
+++ src/pages/api/auth/nextauth.js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth"
+import providers from "next-auth/providers"
+import dotenv from "dotenv"
+
+export default NextAuth({
+    providers: [
+        providers.Google({
+            clientId: dotenv.GOOGLE_ID,
+            clientSecret: dotenv.GOOGLE_SECRET,
+        })
+    ],
+    database: dotenv.DATABASE_URL,
+    secret: dotenv.SECRET,
+    session: {
+        jwt: true,
+        maxAge: 30 * 24 * 60 * 60 // 30 days
+    },
+    jwt: {
+        secret: dotenv.JWT_SECRET,
+        encryption: true,
+        signingKey: dotenv.JWT_SIGNING_PRIVATE_KEY,
+        encryptionKey: dotenv.JWT_ENCRYPTION_PRIVATE_KEY,
+        encryptionAlgorithm: "RS256"
+    },
+    pages: {
+        signIn: "api/auth/signin",
+        signOut: "api/auth/signout",
+        error: "api/auth/error", // Error code passed in query string as ?error=
+        verifyRequest: "/auth/verify-request", // (used for check email message)
+        newUser: null // If set, new users will be directed here on first sign in
+    },
+    callbacks: {
+        async jwt(token, user, account, profile, isNewUser) {
+            // Add access_token to the token right after signin
+            if (account?.accessToken) {
+                token.accessToken = account.accessToken
+            }
+            return token
+        }
+    }
+})
